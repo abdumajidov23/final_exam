e6d8864036142f9d7cc94d65282e6eb0091a5c66
abdumajidov23 <alijonabdumajidov09@gmail.com>
2025-02-11 05:30:27 +0500

    update_MovingText

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,11 @@
 import Hero from "../../components/hero/Hero"
-import MovingText from "../../components/MovingText";
 import Products from "../../components/products/Products"
 import { useGetProductsQuery } from "../../redux/api/product-api";
 import HomeLoading from "../loading/HomeLoading";
 
 const Home = () => {
   const { data, isLoading } = useGetProductsQuery({});
-  
+
 
   return (
     <div className="container">
@@ -18,8 +17,8 @@ const Home = () => {
           <Hero />
           {data && (
             <Products
-            data={data}
-            title=""
+              data={data}
+              title=""
             />
           )}
         </>
